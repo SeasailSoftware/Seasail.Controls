421757a390dda7208eef202e8622b0ee5f148074
Seasail <331893296@qq.com>
2023-04-14 14:48:26 +0800

    add dashboard project

---
+++ Seaasail.Controls.Demo/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Seaasail.Controls.Demo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Begin(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+           {
+               this.Dispatcher.Invoke(new Action(() =>
+               {
+                   dashboard.Value = 0;
+               }));
+               var index = 0;
+               while (index < 100)
+               {
+                   this.Dispatcher.Invoke(new Action(() =>
+                   {
+                       dashboard.Value++;
+                   }));
+                   Thread.Sleep(100);
+                   index++;
+               }
+
+           });
+        }
+    }
+}
